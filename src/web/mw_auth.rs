22e8093eec768148b31c668e6994d6c203e15b05
kurbatov <kurbatxx@gmail.com>
2023-05-17 17:23:10 +0600

    add ctx

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,28 +1,50 @@
+use async_trait::async_trait;
+use axum::extract::FromRequestParts;
+use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
+use axum::RequestPartsExt;
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
+use crate::ctx::Ctx;
 use crate::error::{Error, Result};
 use crate::web::AUTH_TOKEN;
 
 pub async fn wm_require_auth<B>(
-    cookies: Cookies,
+    ctx: Result<Ctx>,
     req: Request<B>,
     next: Next<B>,
 ) -> Result<Response> {
-    println!("-> {:<12} - wm_require_auth", "MIDDLEWARE");
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+    println!("-> {:<12} - wm_require_auth - {ctx:?}", "MIDDLEWARE");
 
-    // Parse token
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
-
-    // TODO: Token components validation.
+    ctx?;
 
     Ok(next.run(req).await)
+}
+
+// Ctx Extractor
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("-> {:<12} - Ctx", "EXTRACTOR");
+
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+        // Parse token
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        // TODO: Token components validation
+
+        Ok(Ctx::new(user_id))
+    }
 }
 
 /// Parse a token of format user-[user-id].[expiration].[signature]
