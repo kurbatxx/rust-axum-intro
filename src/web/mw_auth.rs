2800739f32198348b91261140785820accf5965d
kurbatov <kurbatxx@gmail.com>
2023-05-17 17:57:45 +0600

    add ctx_resolver

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,15 +1,17 @@
+use crate::{Error, Result};
 use async_trait::async_trait;
-use axum::extract::FromRequestParts;
+use axum::extract::{FromRequestParts, State};
 use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
 use axum::RequestPartsExt;
 use lazy_regex::regex_captures;
-use tower_cookies::Cookies;
+use tower_cookies::{Cookie, Cookies};
 
 use crate::ctx::Ctx;
-use crate::error::{Error, Result};
+
+use crate::model::ModelController;
 use crate::web::AUTH_TOKEN;
 
 pub async fn wm_require_auth<B>(
@@ -20,6 +22,38 @@ pub async fn wm_require_auth<B>(
     println!("-> {:<12} - wm_require_auth - {ctx:?}", "MIDDLEWARE");
 
     ctx?;
+
+    Ok(next.run(req).await)
+}
+
+pub async fn mw_ctx_resolver<B>(
+    _mc: State<ModelController>,
+    cookies: Cookies,
+    mut req: Request<B>,
+    next: Next<B>,
+) -> Result<Response> {
+    println!("-> {:12} - mw_ctx_resolver", "MIDDELWARE");
+
+    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+    let result_ctx = match auth_token
+        .ok_or(Error::AuthFailNoAuthTokenCookie)
+        .and_then(parse_token)
+    {
+        Ok((user_id, _exp, _sign)) => {
+            // TODO: Token component validations
+            Ok(Ctx::new(user_id))
+        }
+
+        Err(e) => Err(e),
+    };
+
+    // Remove the cookie if something wrong other than NoAuthTokenCookie
+    if result_ctx.is_err() && !matches!(result_ctx, Err(Error::AuthFailNoAuthTokenCookie)) {
+        cookies.remove(Cookie::named(AUTH_TOKEN))
+    }
+
+    // Store the ctx_result in the request extention
+    req.extensions_mut().insert(result_ctx);
 
     Ok(next.run(req).await)
 }
